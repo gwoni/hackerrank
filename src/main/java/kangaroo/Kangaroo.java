535325d793e6f0bae450d3d864d8c5e7905d091e
gwoni <gwoni52@gmail.com>
2020-01-09 00:51:22 +0900

    함수구분

--- src/main/java/kangaroo/Kangaroo.java
+++ src/main/java/kangaroo/Kangaroo.java
@@ -2,10 +2,41 @@ package kangaroo;
 
 public class Kangaroo {
 
+    /**
+     * 두 캥거루가 만날수 있는지 확인
+     * @param x1 캥거루1의 위치
+     * @param v1 캥거루1의 점프거리
+     * @param x2 캥거루2의 위치
+     * @param v2 캥거루2의 점프거리
+     * @return 두 캥거루가 만나면  YES, 만날수 없으면  NO값 출력
+     */
     public String solve(int x1, int v1, int x2, int v2) {
-        if(v1 < v2 || v1 == v2) return "NO";
-        if((x2 - x1) % (v1 - v2) == 0 ) return "YES";
+        if(limitationCheck(v1,v2)) return "NO";
+        if(sameTimeSite(x1, v1, x2, v2)) return "YES";
         return "NO";
     }
 
+    /**
+     * 제약조건에서 캥거루2의 점프거리가 캥거루1보다 커야되며 같은 위치이지 속도가 달라야지 두 캥거루가 만날수 있다
+     * @param v1 캥거루 1의 점프거리
+     * @param v2 캥거루 2의 점프 거리
+     * @return  두 캥거루가 만날 조건 확인
+     */
+    public boolean limitationCheck(int v1, int v2){
+        if(v1 < v2 || v1 == v2) return true;
+        else return false;
+    }
+
+    /**
+     * 두 캥거루가 만날수 있는지 계산
+     * @param x1 캥거루1의 위치
+     * @param v1 캥거루1의 점프거리
+     * @param x2 캥거루2의 위치
+     * @param v2 캥거루2의 점프거리
+     * @return 두 캥거루가 만날 수 있는지  true, false
+     */
+    public boolean sameTimeSite(int x1, int v1, int x2, int v2){
+        if((x2 - x1) % (v1 - v2) == 0 ) return true;
+        else return false;
+    }
 }
--- src/test/java/kangaroo/KangarooTest.java
+++ src/test/java/kangaroo/KangarooTest.java
@@ -14,14 +14,29 @@ public class KangarooTest {
         this.kangaroo = new Kangaroo();
     }
 
+    /**
+     * https://www.hackerrank.com/challenges/kangaroo/problem
+     * 캥거루 1은 x1 위치에서 v1 미터를 점프하여 이동
+     * 캥거루 2는 x2 위치에서 v2 미터를 점프하여 이동
+     * 두 캥거루가 같은 점프 횟수로 같은 위치에 있으면 YES 값을 아니면 NO 값을 출력
+     *
+     * 제약 조건
+     * 0<=x1<x2<=10000
+     * 1<=v1<=10000
+     * 1<=v2<=10000
+     */
     @Test
     public void kangarooTest() {
-        int [] intArray = { 0, 3, 4, 2 };
+        int x1 = 0, v1 = 3; // 캥거루1의 위치와 한번 점프 이동거리
+        int x2 = 4, v2 = 2; // 캥거루2의 위치와 한번 점프 이동거리
+
+        int [] intArray = {x1, v1, x2, v2};
         String result = kangaroo.solve(intArray[0], intArray[1], intArray[2], intArray[3]);
         String ecpected = "YES";
         assertThat(result, is(ecpected));
     }
-/*
+
+/* 간결 코드 모드
     private String kangaroo(int x1, int v1, int x2, int v2) {
         if(v1 < v2 || v1 == v2) return "NO";
         if((x2 - x1) % (v1 - v2) == 0 ) return "YES";
